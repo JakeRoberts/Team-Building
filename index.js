e8040159c32c5641891e2b92f0ca7a6f7f1cc516
Rob <r.jacobs790@gmail.com>
2021-04-17 17:50:29 -0400

    updated employee test file

---
+++ index.js
@@ -0,0 +1,18 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+//Starts the app
+ const menu = () => {
+     createManager = () => {
+         inquirer.prompt([
+             
+         ])
+     }
+ }
+
+
+ menu();
